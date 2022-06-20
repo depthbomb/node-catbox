c088864853cc3daf1fab26c60eab728706612a10
depthbomb <reesepup77@hotmail.com>
2022-06-20 17:44:50 -0500

    chore: update user-agent

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const USER_AGENT = 'depthbomb/node-catbox (based on node-fetch)' as const;
-export const CATBOX_BASE_URL = 'https://catbox.moe/user/api.php' as const;
+export const USER_AGENT         = 'depthbomb/node-catbox' as const;
+export const CATBOX_BASE_URL    = 'https://catbox.moe/user/api.php' as const;
 export const LITTERBOX_BASE_URL = 'https://litterbox.catbox.moe/resources/internals/api.php' as const;
